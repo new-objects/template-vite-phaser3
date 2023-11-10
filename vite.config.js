9af12b2734d2779b8e1ca9c86eb1cfa5eb73a0e4
mi-skam <maksim@miskam.xyz>
2023-11-10 09:28:48 +0100

    fine-tuning npm build for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
   base:
-    process.env.NODE_ENV === 'production' ? '/template-frontend-phaser3/' : '/',
+    process.env.DEPLOY_ENV === 'gh-pages' ? '/template-frontend-phaser3/' : '/',
   plugins: [
     eslintPlugin({
       cache: false,
-      failOnError: false
+      failOnError: false,
     }),
   ],
   assetsInclude: ['**/*.mp3', '**/*.jpeg', '**/*.png', '**/*.task'],
